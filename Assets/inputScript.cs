89127c11c9bd95f906703109c7403a9fce72f005
Ignacio Nuñez Valcarce <ignacionunezvalcarce@gmail.com>
2024-05-27 13:57:30 -0300

    Add Complete

---
+++ Assets/inputScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputScript : MonoBehaviour
+{
+    public InputField passwordUser;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void acceso()
+    {
+        if (passwordUser.text == "Nacho2024")
+        {
+            Debug.Log("Access granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
+    }
+}
